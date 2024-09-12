d2a59d4962d46cb7e8fc1cb27ce8c43f53998bda
shaban <146794146+sarzhh@users.noreply.github.com>
2024-09-12 01:55:41 -0700

    Display the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,12 @@
-class board 
+class Board 
   def initialize
   @board = Array.new(3) {Array.new(3, "")}
   end
 
-  
+  def display
+    @board.each do |row|
+      puts row.join(' | ')
+      puts '-------' unless row.equal?(@board.last)
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
 require_relative "lib/board"
 
 my_board = Board.new
+my_board.display

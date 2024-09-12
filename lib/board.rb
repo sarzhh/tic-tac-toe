bd20e46a046d13a50d4eab19ceed5473083e4384
shaban <146794146+sarzhh@users.noreply.github.com>
2024-09-12 02:08:57 -0700

    Add comments

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
+
 class Board 
+  # Create a board
   def initialize
   @board = Array.new(3) {Array.new(3, "")}
   end
 
+  # Display the board
   def display
     @board.each do |row|
       puts row.join(' | ')
